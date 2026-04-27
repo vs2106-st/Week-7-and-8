f95228a9767e8434ef50df97a56f7986d09a549a
vs2106-st <vs3697@srmist.edu.in>
2026-04-27 22:49:01 +0530

    Create SimpleInterest.java

---
+++ SimpleInterest.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SimpleInterest{
+
+    public static double calculateSI(double p, double r, double t) {
+        return (p * r * t) / 100;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double principal = sc.nextDouble();
+        double rate = sc.nextDouble();
+        double time = sc.nextDouble();
+
+        double si = calculateSI(principal, rate, time);
+
+        System.out.println("Simple Interest = " + si);
+    }
+}
