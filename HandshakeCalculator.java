3fc624ecad9c30e20a6f61e86e1b568cdb03bce2
vs2106-st <vs3697@srmist.edu.in>
2026-04-27 22:47:22 +0530

    Create HandshakeCalculator.java

---
+++ HandshakeCalculator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class HandshakeCalculator {
+
+    public static int calculateHandshakes(int n) {
+        return (n * (n - 1)) / 2;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int students = sc.nextInt();
+
+        System.out.println("Handshakes = " + calculateHandshakes(students));
+    }
+}
